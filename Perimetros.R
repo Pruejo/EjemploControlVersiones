787eab3ce6d1e5473c12da59647444db136fee3e
Pruejo <pruejo@alumni.uv.es>
2026-02-05 18:46:46 +0100

    Longitud circunferencia

--- Perimetros.R
+++ Perimetros.R
@@ -1,6 +1,14 @@
 AreaCuadrado = function(L){
   return(4*L)
 }   
+
+PerimetroCircunferencia = function(R){
+  return(3.14159*2*R)
+}   
+
+
+
+
 AreaRectangulo = function(L,H){
   return(2*L + 2*H)
 }   
